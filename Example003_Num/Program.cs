d19dda5f869890373d5970b43ba212f4aed5d866
Ascold87 <AscoldSamurov@gmail.com>
2022-09-19 17:29:30 +0300

    modified Ex003

--- Example003_Num/Program.cs
+++ Example003_Num/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberА = 43;
-int numberB = 67;
+﻿int numberА = new Random().Next(1,10); // 1 2 3 ... 9
+Console.WriteLine("1th is - " + numberА);
+int numberB = new Random().Next(1,10); 
+Console.WriteLine("2th is - " + numberB);
 int result = numberА + numberB;
-Console.WriteLine(result);
+Console.WriteLine("Sum = " + result);
 
