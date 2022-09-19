b6d27f143392f60017b2c6a6a4a1bd973587d4d4
Ascold87 <AscoldSamurov@gmail.com>
2022-09-19 03:26:46 +0300

    Added Ex002 Hello user

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введи своё имя, смертный!!! -  ");
+string username = Console.ReadLine();
+Console.Write("Какое заурядное имя, ");
+Console.Write(username);
+Console.Write(". А-ха-ха. Ты пришел ко мне в услужение?");
