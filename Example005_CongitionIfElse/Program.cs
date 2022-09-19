41b8d875aca96b375c918ee19225bfc6b35ba0b0
Ascold87 <AscoldSamurov@gmail.com>
2022-09-19 18:05:58 +0300

    Add Ex005 If_else

---
+++ Example005_CongitionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Назовись, смертный!!! - ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ascold")
+{
+    Console.WriteLine("Приветствую тебя, мой лорд!!!");
+}
+else
+{
+    Console.Write("Что привело тебя сюда, ");
+    Console.Write(username);
+    Console.WriteLine(" ?");
+}
