6e91c4bbc8386a16ab0b41c7e75da0b71fd56e4c
Ascold87 <AscoldSamurov@gmail.com>
2022-09-22 18:12:59 +0300

    Add HW1 Ex6

---
+++ HW-1_Ex-6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число.");
+int user_A = int.Parse(Console.ReadLine());
+
+if (user_A % 2 == 0)
+{
+    Console.Write("user_A");
+    Console.WriteLine(" - Четное.");
+}
+else
+{
+    Console.Write("user_A");
+    Console.WriteLine(" - Нечетное.");
+}
+
