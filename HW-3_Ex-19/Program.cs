7b4c7fe525f7c46d1f3cb3f33a044c44e7a2a042
Ascold87 <AscoldSamurov@gmail.com>
2022-09-29 19:07:59 +0300

    Add HW-3_Ex-19

--- HW-3_Ex-19/Program.cs
+++ HW-3_Ex-19/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int user_number(int num, int NumberOfFirstNumbers)
+{
+    while (num / Math.Pow(10, NumberOfFirstNumbers) > 1)
+    {
+        num /= 10;
+    }
+    return num;
+}
+
+Console.Write("Введите 5ти-значное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    int x = number % 100;
+    int y = user_number(number, 2);
+
+    if (x / 10 == y % 10 && x % 10 == y / 10)
+    {
+        Console.WriteLine("Это число - палидром.");
+    }
+    else
+    {
+        Console.WriteLine("Это число - не палидром.");
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число.");
+}
