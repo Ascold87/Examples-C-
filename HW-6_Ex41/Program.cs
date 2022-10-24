e00ce54caa38862c8eb492e3ce131e6892873d1d
Ascold87 <AscoldSamurov@gmail.com>
2022-10-24 17:58:00 +0300

    Add HW6 Ex41, 43
    Signed-off-by: Ascold87 <AscoldSamurov@gmail.com>

---
+++ HW-6_Ex41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string ReadData(string msg)
+{
+    Console.Write(msg);
+    string numbers = Console.ReadLine() ?? "0";
+    return numbers;
+}
+
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+
+string numbers = ReadData("Введите с клавиатуры через пробел нескольео чисел: ");
+string[] array = numbers.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int count = 0;
+foreach (var item in array)
+{
+    if (int.Parse(item) > 0) count++;
+}
+PrintData($"Количество цифр больше нуля = ", count);
---
+++ HW-6_Ex43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = a1 * x + b1, y = a2 * x + b2; значения b1, а1, b2 и а2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintData(string msg, string point)
+{
+    Console.WriteLine(msg + point);
+}
+
+string IntersectionPoint(double numA1, double numB1, double numA2, double numB2)
+{
+    double x = ((numB2 - numB1) / (numA1 - numA2));
+    double y = (numA1 * ((numB2 - numB1) / (numA1 - numA2)) + numB1);
+    return $"({x}; {y})";
+}
+
+int numA1 = ReadData("Поиск точки пересечения двух прямых, заданных функциями [y1 = a1 * x + b1] и [y2 = a2 * x + b2]. Введите первую точку первого отрезка (а1): ");
+int numB1 = ReadData("Введите вторую точку первого отрезка (b1): ");
+int numA2 = ReadData("Введите первую точку второго отрезка (а2): ");
+int numB2 = ReadData("Введите вторую точку второго отрезка (b2): ");
+
+string point = IntersectionPoint(numA1, numB1, numA2, numB2);
+PrintData("Точка пересечения двух прямых находится в координатах ", point);
