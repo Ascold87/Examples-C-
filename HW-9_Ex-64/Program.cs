95bc34d9e4495a4de8beef5a26cbb4bd51a86a98
Ascold87 <AscoldSamurov@gmail.com>
2022-11-01 19:16:10 +0300

    Add HW-9

---
+++ HW-9_Ex-64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+void PrintData1(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void PrintData2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void Numbers(int A) // тут идёт задание рекурсии
+{
+    while (A > 0)
+    {
+        Console.WriteLine(A);
+        Numbers(A - 1);
+        break;
+    }
+}
+
+PrintData1("Программа, вычисляет и выводит в столбик все натуральные числа в промежутке от N до 1.");
+int N = ReadData("Задайте значение N: ");
+PrintData2($"Натуральные числа в промежутке от {N} до 1: ");
+Numbers(N);
---
+++ HW-9_Ex-66/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+void PrintData1(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+void PrintData2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int Numbers(int A, int B)
+{
+    int sum = 0;
+    if (A > B)
+    {
+        int a = (A * (A + 1)) / 2;
+        int b = (B * (B + 1)) / 2;
+        sum = a - b + B;
+        return sum;
+    }
+    else
+    {
+        int a = (B * (B + 1)) / 2;
+        int b = (A * (A + 1)) / 2;
+        sum = a - b + A;
+        return sum;
+    }
+}
+
+PrintData2("Программа вычисляет сумму всех натуральных чисел в интервале от M до N.");
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+int result = Numbers(M, N);
+PrintData1($"Сумма всех натуральных чисел в интервале от {M} до {N}: ", result);
